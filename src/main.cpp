1af7e17324e9a53a44ba6bc1bef0429c5ddee5d9
PaulPetuschenko <55639625+PaulPetuschenko@users.noreply.github.com>
2020-05-29 16:45:43 +0200

    SD-card support

--- src/main.cpp
+++ src/main.cpp
@@ -1,79 +1,103 @@
 #include <Arduino.h>
-#include <SPI.h>
 #include <Ethernet.h>
+#include <SPI.h>
+#include <SD.h>
 
-byte mac[] = {
-  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED 
-};
-
-IPAddress ip(192,168,178,176);
+byte mac[] = {0x90, 0xA2, 0xDA, 0x10, 0xD6, 0xDA};
 
 EthernetServer server(80);
 
+File webFile;
+
+int LED = 2;
+
 void setup() {
-  // put your setup code here, to run once:
+
+  // serial begin
   Serial.begin(9600);
   while (!Serial)
   {
     ;
-  }
-  
+  } 
 
-  Ethernet.begin(mac, ip);
+  // arduino pins init
+  pinMode(LED,OUTPUT);
+
+  // server begin
+  Ethernet.begin(mac);
   server.begin();
   Serial.print("Server is at ");
   Serial.println(Ethernet.localIP());
+
+  //SD-card init
+  if (!SD.begin())
+  {
+    Serial.println("Error - SD initialization failed!");
+    return;
+  }
+  
+  if (!SD.exists("index.htm"))
+  {
+    Serial.println("Error - there is no such file or directory!");
+    return;
+  }
+
+  Serial.println("Success");
+
 }
 
 void loop() {
-  // Listen for clients
   EthernetClient client = server.available();
-  if (client)
+
+  if (client) 
   {
-    Serial.println("new client");
-    // http request
     boolean currentLineIsBlank = true;
     while (client.connected())
     {
       if (client.available())
       {
         char c = client.read();
-        Serial.write(c);
-
 
         if (c == '\n' && currentLineIsBlank)
-        {
+          {
+
           client.println("HTTP/1.1 200 OK");
           client.println("Content-Type: text/html");
           client.println("Connection: close");
           client.println();
-          client.println("<!DOCTYPE HTML>");
-          client.println("<html>");
-          client.println("<meta http-equiv=\"refresh\" content=\"5\">");
-                    
-          for (int analogChannel = 0; analogChannel < 6; analogChannel++) {
-            int sensorReading = analogRead(analogChannel);
-            client.print("analog input ");
-            client.print(analogChannel);
-            client.print(" is ");
-            client.print(sensorReading);
-            client.println("<br />");       
+
+          webFile = SD.open("index.htm");
+
+          if (webFile) {
+            while (webFile.available())
+            {
+              client.write(webFile.read());
+            }
+            webFile.close();
+            
           }
-          client.println("</html>");
+          else
+          {
+            Serial.println("Error while loading index.htm");
+          }
+          
+
           break;
+        }
+
+        if (c == '\n')
+        {
+          currentLineIsBlank = true;
         }
         
         if (c != '\r')
         {
           currentLineIsBlank = false;
         }
-        
       }
+      
     }
-
     delay(1);
-
     client.stop();
-    Serial.println("client disonnected");
-  }
+  } 
 }
